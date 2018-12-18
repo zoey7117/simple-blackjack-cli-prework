ae25008026d9dd0694737132cd7cae7bc3230bb7
cyn7117 <cyn7117@gmail.com>
2018-12-18 00:47:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,8 +31,18 @@ def initial_round
   sum
 end
 
-
-
+def hit?(total)
+  prompt_user
+  user_input = get_user_input
+  if(user_input == 'h')
+    total += deal_card
+    elsif(user_input == 's')
+    total
+  else
+    invalid_command
+  end
+  total
+end
 
 
 
